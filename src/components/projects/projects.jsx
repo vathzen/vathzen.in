32a78a793f7de2c8cff87be7033a352f23f6ecad
Sri Vathsan <srivaths311@gmail.com>
2023-12-01 14:55:36 +0000

    new resume

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -18,6 +18,7 @@ const Projects = (props) => {
             <div className="body">
                 <div className="desc">
                     <p>Listed are the various personal projects and Projects I have worked in my college over the years.</p>
+                    <p style={{fontSize:'1.5vh'}}>Skip to the fun ones ></p>
                 </div>
 
                 <div className="projects-outer">
@@ -25,7 +26,7 @@ const Projects = (props) => {
                         <div className="project">
                             <div className='sub-title'>iGEM 2019</div>
                             <div className="logo">
-                                <img src={iGEM} alt="" height="200px"/>
+                                <img src={iGEM} alt="" height="200px" className='iGem'/>
                                 <img src={TeamPic} alt="" height="350px" />
                             </div>
                             <div className="proj-info">
@@ -33,9 +34,9 @@ const Projects = (props) => {
                                 <p>Developed the Project Website and Machine Learning Classifier for identifying optimal toehold switch design based on physical parameters towards detection of cervical cancer markers.</p>
                                 <p>Co-Ordinated Social Media Outreach and PR for the project.</p>
                                 <div className="project-links">
-                                    Project Website :&nbsp;<a href="https://2019.igem.org/Team:SASTRA_Thanjavur"> SASTRA Thanjavur iGEM 2019</a>&nbsp;
-                                    Toehold Design :&nbsp;<a href="https://sas.sastra.edu/rnatoedesign/"> Toehold Designer</a>&nbsp;
-                                    Instagram :&nbsp;<a href="https://www.instagram.com/igem_sastra/"> iGEM @ Sastra </a>
+                                    <div>Project Website :&nbsp;<a href="https://2019.igem.org/Team:SASTRA_Thanjavur"> SASTRA Thanjavur iGEM 2019</a>&nbsp;</div>
+                                    <div>Toehold Design :&nbsp;<a href="https://sas.sastra.edu/rnatoedesign/"> Toehold Designer</a>&nbsp;</div>
+                                    <div>Instagram :&nbsp;<a href="https://www.instagram.com/igem_sastra/"> iGEM @ Sastra </a></div>
                                 </div>
                             </div>                           
                         </div>
@@ -46,14 +47,16 @@ const Projects = (props) => {
                                 <p>Sastra Mess Food Booking App connects Catering Services to fingertips of Students, allowing them to book the A La Carte Menu Offered from anywhere.</p>
                                 <p>Developed the REST API and the Database Structure for the project with 2 Frontends; Admin Interface and Student Interface.</p>
                                 <p>Automatically interfaces with Student Web Portal to show updated data.</p>
-                                <div className="project-links-col">
-                                    Project Website :&nbsp;<a href="https://yumsastra.vathzen.in/"> Yum - Sastra</a>&nbsp; <br />
-                                    Github :
-                                    <ul>
-                                        <li>Backend : <a href="https://github.com/vathzen/mess-backend">mess-backend</a></li>
-                                        <li>Student App : <a href="https://github.com/vathzen/SASTRAMess">mess-backend</a></li>
-                                        <li>Admin Front End : <a href="https://github.com/vathzen/WebAppMess">mess-backend</a></li>
-                                    </ul>
+                                <div className="project-links">
+                                    <div>Project Website :&nbsp;<a href="https://yumsastra.vathzen.in/"> Yum - Sastra</a>&nbsp;</div>
+                                    <div>
+                                        Github :
+                                        <ul>
+                                            <li>Backend : <a href="https://github.com/vathzen/mess-backend">mess-backend</a></li>
+                                            <li>Student App : <a href="https://github.com/vathzen/SASTRAMess">Yum SASTRA</a></li>
+                                            <li>Admin Front End : <a href="https://github.com/vathzen/WebAppMess">Mess WebApp</a></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>                           
                         </div>
