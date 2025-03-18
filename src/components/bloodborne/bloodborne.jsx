1b14738462b78805bca83ccbb729dc22c7799789
Sri Vathsan <srivaths311@gmail.com>
2025-03-18 19:40:22 +0200

    bloodborne install

---
+++ src/components/bloodborne/bloodborne.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Bloodborne = (props) => {
+    return (
+        <ol>
+            <li>Get Diegolix version of ShadPS4 here: <a href='https://github.com/diegolix29/shadPS4/releases/download/release-shadPS4-2025-03-13-dfa424f/shadps4-win64-qt-2025-03-13-dfa424f.zip'>ShadPS4</a></li>
+            <li>Get Bloodborne from here: <a href='magnet:?xt=urn:btih:0bb324a77b30c7ab2ca91625fe45a56b0a684e7e&dn=Bloodborne.Game.of.the.Year.Edition.PS4-PRELUDE&tr=http://tracker.trackerfix.com:80/announce&tr=udp://9.rarbg.me:2710&tr=udp://9.rarbg.to:2710'>Magnet Link</a> </li>
+
+            <li>Installing ShadPS4 is pretty straightforward you just Unzip it. Give Game and DLC Install location as the same folder.</li>
+            <li>File - Install Packages - Choose the Bloodborne main package. May take a while depending on you PC.</li>
+            <li>File - Install Packages - Choose the Bloodborne DLC package. The GUI must change to Version 1.09</li>
+            <li>Right click the game - Cheats and Patches - Patches Tab - Bottom Dropdown choose ShadPS4 - Download Patches</li>
+            <li>
+                I used the following patches:
+                <ul>
+                    <li>Skip Intro</li>
+                    <li>Disable Motion Blur</li>
+                    <li>Disable HTTP Requests</li>
+                    <li>60 FPS with Deltatime</li>
+                    <li>Force Enable Old Hunters DLC</li>
+                    <li>I installed a resolution patch to match my monitor, but it caused me crashes after I kill the first enemy, so try at your risk.</li>
+                </ul>
+            </li>
+            <li>Create an account in Nexusmods here: <a href="https://www.nexusmods.com/">Nexus Mods</a></li>
+            <li>Go to the Bloodborne page; Sort by Downloads decreasing: <a href='https://www.nexusmods.com/bloodborne/mods/?BH=0'>Bloodborne</a></li>
+            <li>
+                I use the following mods:
+                <ul>
+                    <li><a href='https://www.nexusmods.com/bloodborne/mods/109'>Vertex Explosion Fix !IMPORTANT</a></li>
+                    <li><a href="https://www.nexusmods.com/bloodborne/mods/27">FPS Boost Mod. Kind of Important.</a></li>
+                    <li><a href="https://www.nexusmods.com/bloodborne/mods/70">Some crash fix mod people said worked for them in reddit</a></li>
+                    <li><a href="https://www.nexusmods.com/bloodborne/mods/41">Some crash fix mod people said worked for them in reddit 2</a></li>
+                    <li><a href="https://www.nexusmods.com/bloodborne/mods/30">XBox Controller Layout</a></li>
+                </ul>
+            </li>
+            <li>Last Step: ShadPS4 settings
+                <ol>
+                    <li>Enable Full Screen</li>
+                    <li>Enable Discord Rich Presence</li>
+                    <li>Check Graphics Card in Graphics Tab</li>
+                    <li>In the controller settings I swapped R2 and R1 because I like attacks on Right Trigger, Heavy attacks on Right Bumper.</li>
+                </ol>
+            </li>
+            <li>Last Important Step: <b><u>Put Stream</u></b></li>
+        </ol >
+    )
+}
+
+export default Bloodborne;
--- src/components/routes/routes.jsx
+++ src/components/routes/routes.jsx
@@ -5,6 +5,7 @@ import Home from '../home/home';
 import Me from '../me/me';
 import Experience from '../experiences/experiences';
 import Projects from '../projects/projects';
+import Bloodborne from '../bloodborne/bloodborne'
 
 export default function Routes() {
     return (
@@ -15,6 +16,7 @@ export default function Routes() {
             <Route exact path='/experience' component={Experience} />
             <Route exact path='/interests' component={Home} />
             <Route exact path='/designs' component={Home} />
+            <Route exact path='/bb' component={Bloodborne} />
 
             <Route exact path='/discord' component={() => {
                 window.location.replace('https://discord.gg/MZFaHMJbtU');
